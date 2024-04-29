3ad1f5d6382906b7ef84c095a4b77355a8c34e91
kotomoto <jkotor@gmail.com>
2024-04-29 11:23:56 +0200

    Add fetching items from firebase

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,22 +15,47 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url =
+        Uri.https('flutter-shoplist-groceries-default-rtdb.firebaseio.com', 'shopping-list.json');
+
+    final response = await http.get(url);
+
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere((catItem) => catItem.value.title == item.value['category'])
+          .value;
+      _loadedItems.add(GroceryItem(
+        id: item.key,
+        name: item.value['name'],
+        quantity: item.value['quantity'],
+        category: category,
+      ));
+    }
+
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
 
   void _addItem() async {
-    var newItem = await Navigator.of(context).push<GroceryItem>(
+    await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
 
-    if (newItem == null) {
-      return;
-    }
-
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
@@ -44,11 +74,11 @@ class _GroceryListState extends State<GroceryList> {
       content = ListView.builder(
           itemCount: _groceryItems.length,
           itemBuilder: (ctx, index) => Dismissible(
-            key: ValueKey(_groceryItems[index].id),
-            onDismissed: (direction) {
-              _removeItem(_groceryItems[index]);
-            },
-            child: ListTile(
+                key: ValueKey(_groceryItems[index].id),
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+                child: ListTile(
                   title: Text(_groceryItems[index].name),
                   leading: Container(
                     width: 24,
@@ -59,7 +89,7 @@ class _GroceryListState extends State<GroceryList> {
                     _groceryItems[index].quantity.toString(),
                   ),
                 ),
-          ));
+              ));
     }
 
     return Scaffold(
